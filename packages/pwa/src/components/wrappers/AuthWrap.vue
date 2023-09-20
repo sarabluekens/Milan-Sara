184c40a79f189b7052f2029fbaede08c86d1e537
DeMeulemeesterMilan <Milan.De.Meulemeester@student.howest.be>
2023-09-20 14:51:19 +0200

    feat: Added wrapper

---
+++ packages/pwa/src/components/wrappers/AuthWrap.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="flex items-center w-full max-w-xl mx-auto pb-48 px-6">
+    <RouterView />
+  </div>
+</template>
--- packages/pwa/src/composables/useFirebase.ts
+++ packages/pwa/src/composables/useFirebase.ts
@@ -4,12 +4,12 @@ import { ref } from 'vue'
 
 // Shared state
 const app = initializeApp({
-  apiKey: import.meta.env.VITE_apiKey,
-  authDomain: import.meta.env.VITE_authDomain,
-  projectId: import.meta.env.VITE_projectId,
-  storageBucket: import.meta.env.VITE_storageBucket,
-  messagingSenderId: import.meta.env.VITE_messagingSenderId,
-  appId: import.meta.env.VITE_appId,
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 })
 
 const auth = getAuth(app)
