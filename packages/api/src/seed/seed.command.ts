e2feaff5289742a4a13f9f26573a48b8cffd4acc
sarabluekens <sara.bluekens@skynet.be>
2023-09-26 18:02:03 +0200

    feat: seeding second entity

--- packages/api/src/seed/seed.command.ts
+++ packages/api/src/seed/seed.command.ts
@@ -6,6 +6,7 @@ import { SeedService } from './seed.service'
 export class DatabaseSeedCommand {
   constructor(private readonly seedService: SeedService) {}
 
+  //   seeding for caregivers
   @Command({
     command: 'seed:database:caregivers',
     describe: 'Seed the database with caregivers',
@@ -20,9 +21,30 @@ export class DatabaseSeedCommand {
     command: 'seed:reset:caregivers',
     describe: 'Delete all data from the caregivers table',
   })
-  async delete() {
+  async deleteCaregivers() {
     console.info('🔪 Start deleting caregivers')
     await this.seedService.deleteAllCaregivers()
     console.info('🪶 Removed caregivers')
+  }
+
+  //   seeding for equipments
+  @Command({
+    command: 'seed:database:equipments',
+    describe: 'Seed the database with equipment',
+  })
+  async seedEquipment() {
+    console.info('⛑️ Start seeding of equipment')
+    const equipment = await this.seedService.addEquipmentFromJson()
+    console.info(`⛑️ ${equipment.length} Equipment are added`)
+  }
+
+  @Command({
+    command: 'seed:reset:equipments',
+    describe: 'Delete all data from the equipment table',
+  })
+  async deleteEquipment() {
+    console.info('🔪 Start deleting equipment')
+    await this.seedService.deleteAllEquipment()
+    console.info('🪶 Removed equipment')
   }
 }
--- packages/api/src/seed/seed.service.ts
+++ packages/api/src/seed/seed.service.ts
@@ -2,10 +2,16 @@ import { Injectable } from '@nestjs/common'
 import { CaregiversService } from 'src/caregivers/caregivers.service'
 import { Caregiver } from 'src/caregivers/entities/caregiver.entity'
 import * as caregivers from './data/caregivers.json'
+import * as equipments from './data/equipments.json'
+import { EquipmentsService } from 'src/equipments/equipments.service'
+import { Equipment } from 'src/equipments/entities/equipment.entity'
 
 @Injectable()
 export class SeedService {
-  constructor(private caregiversService: CaregiversService) {}
+  constructor(
+    private caregiversService: CaregiversService,
+    private equipmentsService: EquipmentsService,
+  ) {}
 
   async addCaregiversFromJson(): Promise<Caregiver[]> {
     let theCaregivers: Caregiver[] = []
@@ -24,5 +30,25 @@ export class SeedService {
 
   async deleteAllCaregivers(): Promise<void> {
     return this.caregiversService.truncate()
+  }
+
+  async addEquipmentFromJson(): Promise<Equipment[]> {
+    let theEquipments: Equipment[] = []
+    for (let equipment of equipments) {
+      const b = new Equipment()
+      b.name = equipment.name
+      b.fullname = equipment.fullname
+      b.category = equipment.category
+      b.amount = equipment.amount
+      b.available = equipment.available
+
+      theEquipments.push(b)
+    }
+
+    return this.equipmentsService.saveAll(theEquipments)
+  }
+
+  async deleteAllEquipment(): Promise<void> {
+    return this.equipmentsService.truncate()
   }
 }
