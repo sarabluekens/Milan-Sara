00fd1ca38f23a2b817a7d8a76c0c8dbbf735536e
DeMeulemeesterMilan <Milan.De.Meulemeester@student.howest.be>
2023-09-26 16:28:32 +0200

    feat: entity relation added

--- packages/api/src/equipments/entities/equipment.entity.ts
+++ packages/api/src/equipments/entities/equipment.entity.ts
@@ -1,10 +1,38 @@
 import { ObjectType, Field, ID } from '@nestjs/graphql'
+import { Column, Entity, ObjectIdColumn } from 'typeorm'
 
-@ObjectType()
+@Entity() // Database link - Typeorm
+@ObjectType() //graphql
 export class Equipment {
+  @ObjectIdColumn() // Database link - Typeorm
   @Field(() => ID)
   id: string
 
-  @Field()
+  @Column() // Database link - Typeorm
+  @Field() //graphql
   name: string
+
+  @Column() // Database link - Typeorm
+  @Field() //graphql
+  fullname: string
+
+  @Column() // Database link - Typeorm
+  @Field() //graphql
+  category: string
+
+  @Column() // Database link - Typeorm
+  @Field() //graphql
+  amount: number
+
+  @Column() // Database link - Typeorm
+  @Field() //graphql
+  available: boolean
+
+  @Column() // Database link - Typeorm
+  @Field() //graphql
+  createdAt: Date
+
+  @Column() // Database link - Typeorm
+  @Field() //graphql
+  updatedAt: Date
 }
