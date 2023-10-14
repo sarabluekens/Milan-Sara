a5833d73abe55bb6c96f0d58952f15c3b94c43d6
DeMeulemeesterMilan <Milan.De.Meulemeester@student.howest.be>
2023-10-14 11:14:19 +0200

    feat: updated create input

--- packages/api/src/equipments/dto/create-equipment.input.ts
+++ packages/api/src/equipments/dto/create-equipment.input.ts
@@ -1,19 +1,29 @@
 import { InputType, Int, Field } from '@nestjs/graphql'
+import { IsNotEmpty } from 'class-validator'
 
 @InputType()
 export class CreateEquipmentInput {
+  @IsNotEmpty()
   @Field() //graphql
   name: string
 
-  @Field() //graphql
-  fullname: string
-
+  @IsNotEmpty()
   @Field() //graphql
   category: string
 
-  @Field({ defaultValue: 0 }) //graphql
-  amount: number
-
   @Field() //graphql
+  description?: string
+
+  @Field({ defaultValue: 0 }) //graphql
+  totalStock: number
+
+  @Field(() => [String], { nullable: true }) //graphql
+  reservedStock: string[]
+
+  @Field({ defaultValue: true }) //graphql
   available: boolean
+
+  @IsNotEmpty()
+  @Field() //graphql
+  expirationDate: Date
 }
