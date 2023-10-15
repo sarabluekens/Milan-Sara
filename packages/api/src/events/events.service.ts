24854bb514973c46131a84521c02126951d4ed96
DeMeulemeesterMilan <Milan.De.Meulemeester@student.howest.be>
2023-10-15 13:38:21 +0200

    feat: event service and resolver updated

--- packages/api/src/events/events.service.ts
+++ packages/api/src/events/events.service.ts
@@ -1,26 +1,55 @@
-import { Injectable } from '@nestjs/common';
-import { CreateEventInput } from './dto/create-event.input';
-import { UpdateEventInput } from './dto/update-event.input';
+import { Injectable } from '@nestjs/common'
+import { CreateEventInput } from './dto/create-event.input'
+import { UpdateEventInput } from './dto/update-event.input'
+import { Event } from './entities/event.entity'
+import { InjectRepository } from '@nestjs/typeorm'
+import { Repository } from 'typeorm'
 
 @Injectable()
 export class EventsService {
-  create(createEventInput: CreateEventInput) {
-    return 'This action adds a new event';
+  constructor(
+    @InjectRepository(Event)
+    private readonly eventRepository: Repository<Event>,
+  ) {}
+
+  create(createEventInput: CreateEventInput): Promise<Event> {
+    try {
+      const e = new Event()
+      e.name = createEventInput.name
+      e.address = createEventInput.address
+      e.postalCode = createEventInput.postalCode
+      e.city = createEventInput.city
+      e.dates = createEventInput.dates
+      e.startHour = createEventInput.startHour
+      e.endHour = createEventInput.endHour
+      e.contactPerson = createEventInput.contactPerson
+      e.phoneNumer = createEventInput.phoneNumer
+      e.email = createEventInput.email
+      e.btwNumber = createEventInput.btwNumber
+      e.eventWithChildren = createEventInput.eventWithChildren
+      e.mapsLink = createEventInput.mapsLink
+      e.expectedVisitorStaffCount = createEventInput.expectedVisitorStaffCount
+
+      return this.eventRepository.save(e)
+    } catch (error) {
+      throw error
+    }
   }
 
   findAll() {
-    return `This action returns all events`;
+    return this.eventRepository.find()
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} event`;
+    //@ts-ignore
+    return this.eventRepository.findOneBy({ _id: new ObjectId(id) })
   }
 
   update(id: number, updateEventInput: UpdateEventInput) {
-    return `This action updates a #${id} event`;
+    return `This action updates a #${id} event`
   }
 
   remove(id: number) {
-    return `This action removes a #${id} event`;
+    return `This action removes a #${id} event`
   }
 }
