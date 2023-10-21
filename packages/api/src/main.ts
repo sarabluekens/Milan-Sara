17e124080a8e7d7da1fd054bc200707a3d6f83e9
sarabluekens <sara.bluekens@skynet.be>
2023-10-22 01:11:22 +0200

    feature: cleanup comment code

--- packages/api/src/main.ts
+++ packages/api/src/main.ts
@@ -10,19 +10,13 @@ async function bootstrap() {
       'http://localhost:5173',
       'http://localhost:3001',
       process.env.URL_FRONTEND,
+      //   //staging/TEST
+      //   https://staging-redCross.be,
+
+      //   //PRODUCTION
+      //   http://redCross.be,
     ],
 
-    // origin:
-    // [
-    //   http://localhost:5173,
-    //   http://127.0.0.1:5173,
-
-    //   //staging/TEST
-    //   https://staging-redCross.be,
-
-    //   //PRODUCTION
-    //   http://redCross.be,
-    // ]
     credentials: true,
   }) //<- enable cors for all requests with the right credentials
 
