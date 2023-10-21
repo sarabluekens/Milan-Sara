577dd324aeb735756b1d758c9b5b76f4452b8bb5
sarabluekens <sara.bluekens@skynet.be>
2023-10-22 00:56:29 +0200

    feature: homepage frontend implemented

--- packages/pwa/src/components/generic/AppHeader.vue
+++ packages/pwa/src/components/generic/AppHeader.vue
@@ -1,15 +1,30 @@
 <template>
-  <nav>
-    <ul>
-      <li>
-        <router-link to="/">Home</router-link>
-      </li>
-      <li>
-        <router-link to="/events">Events</router-link>
-      </li>
-      <li>
-        <router-link to="/help">Help</router-link>
-      </li>
-    </ul>
+  <nav v-if="nav === `inversed`" class="nav-beige">
+    <h1 class="hidden">Home</h1>
+
+    <div class="flex flex-col justify-between h-full">
+      <img
+        src="/redCross.svg"
+        alt="Red Cross logo"
+        class="pt-3 w-90% mx-auto"
+      />
+      <img
+        src="/login.svg"
+        alt="login (for Caregivers)"
+        class="pb-3 w-70% mx-auto"
+      />
+    </div>
+  </nav>
+
+  <nav v-else-if="nav === `small`">
+    <h1>small jgnosgbrzog</h1>
+  </nav>
+
+  <nav v-else-if="nav === `wide`">
+    <h1>wide jgnosgbrzog</h1>
   </nav>
 </template>
+
+<script setup lang="ts">
+const nav: string = `inversed`
+</script>
