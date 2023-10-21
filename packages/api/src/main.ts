33800ee4cd6e7aa7e0692c827615d0a0a9fc0305
sarabluekens <sara.bluekens@skynet.be>
2023-10-22 01:06:34 +0200

    hotfix: changing db port

--- packages/api/src/main.ts
+++ packages/api/src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   app.enableCors({
     origin: [
       'http://localhost:5173',
-      'http://localhost:5001',
+      'http://localhost:3001',
       process.env.URL_FRONTEND,
     ],
 
