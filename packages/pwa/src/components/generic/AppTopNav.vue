2b91aba6d2b74ac7426bef6b15652906e319e6a2
DeMeulemeesterMilan <Milan.De.Meulemeester@student.howest.be>
2023-10-26 19:48:46 +0200

    feat: created a topnav file

---
+++ packages/pwa/src/components/generic/AppTopNav.vue
@@ -0,0 +1,8 @@
+<template>
+  <nav>
+    <div class="bg-black h-10 w-10">
+      <p>Dit is de topnav</p>
+      <p>Hoe ga ik dit werkend krijgen vraag ik mij af</p>
+    </div>
+  </nav>
+</template>
