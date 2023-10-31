411f60618788d6d869c89b6d57cc9f77be0de556
DeMeulemeesterMilan <Milan.De.Meulemeester@student.howest.be>
2023-10-31 14:56:36 +0100

    feat: added four simple staff cards

--- packages/pwa/src/views/administration/StaffOverview.vue
+++ packages/pwa/src/views/administration/StaffOverview.vue
@@ -1,9 +1,29 @@
 <template>
   <h1 class="title-black">Staff overview</h1>
-  <section class="flex flex-row justify-center">
-    <div class="border-1 border-red w-1/8 h-10">
+  <section class="flex flex-row mx-auto w-1/2">
+    <div class="border-1 border-red w-1/3 h-10">
       <p>Dropdown</p>
     </div>
-    <input placeholder="Search" class="border-1 border-red w-1/3 h-10 ml-5" />
+    <input placeholder="Search" class="border-1 border-red w-2/3 h-10 ml-3" />
+  </section>
+  <section
+    class="flex flex-row mx-auto my-10 w-1/2 bg-blue justify-between h-16vh"
+  >
+    <div class="text-black w-56 h-18 bg-red text-center">
+      <p class="body-black">Pieter Post</p>
+      <p class="subbody-black">Paramedic</p>
+    </div>
+    <div class="text-black w-56 h-18 bg-red text-center">
+      <p class="body-black">Pieter Post</p>
+      <p class="subbody-black">Paramedic</p>
+    </div>
+    <div class="text-black w-56 h-18 bg-red text-center">
+      <p class="body-black">Pieter Post</p>
+      <p class="subbody-black">Paramedic</p>
+    </div>
+    <div class="text-black w-56 h-18 bg-red text-center">
+      <p class="body-black">Pieter Post</p>
+      <p class="subbody-black">Paramedic</p>
+    </div>
   </section>
 </template>
