6becc39e9cede32a17dd2ab79b5909db7853ff37
DeMeulemeesterMilan <Milan.De.Meulemeester@student.howest.be>
2023-10-31 14:45:31 +0100

    feat: added the layout for the search and dropdown

--- packages/pwa/src/views/administration/StaffOverview.vue
+++ packages/pwa/src/views/administration/StaffOverview.vue
@@ -1,3 +1,9 @@
 <template>
-  <p>Dit is de pagina voor de staffleden te zien</p>
+  <h1 class="title-black">Staff overview</h1>
+  <section class="flex flex-row justify-center">
+    <div class="border-1 border-red w-1/8 h-10">
+      <p>Dropdown</p>
+    </div>
+    <input placeholder="Search" class="border-1 border-red w-1/3 h-10 ml-5" />
+  </section>
 </template>
