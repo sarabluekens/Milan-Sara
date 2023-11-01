3c76f86fabf6bcd90866251923b1aa9581d0d4ae
DeMeulemeesterMilan <Milan.De.Meulemeester@student.howest.be>
2023-11-01 18:47:43 +0100

    feat: created a simple form

--- packages/pwa/src/views/administration/events/AddEvent.vue
+++ packages/pwa/src/views/administration/events/AddEvent.vue
@@ -1,5 +1,96 @@
 <template>
-  <div>
-    <p>Dit is de add event pagina</p>
+  <div class="ml-3.5rem md:ml-5rem bg-beige h-100vh">
+    <h1 class="title-black">New event</h1>
+    <section class="grid grid-cols-2 mx-auto w-1/2">
+      <label>Event name</label>
+      <input
+        type="text"
+        placeholder="Event name"
+        class="border-1 border-red w-2/3 h-10 ml-3 bg-beige"
+      />
+      <label>Address</label>
+      <input
+        type="text"
+        placeholder="Address"
+        class="border-1 border-red w-2/3 h-10 ml-3 bg-beige"
+      />
+      <label>PostalCode</label>
+      <input
+        type="text"
+        placeholder="PostalCode"
+        class="border-1 border-red w-2/3 h-10 ml-3 bg-beige"
+      />
+      <label>City</label>
+      <input
+        type="text"
+        placeholder="City"
+        class="border-1 border-red w-2/3 h-10 ml-3 bg-beige"
+      />
+      <label>Dates</label>
+      <input
+        type="date"
+        placeholder="Dates"
+        class="border-1 border-red w-2/3 h-10 ml-3 bg-beige"
+      />
+      <label>Start time</label>
+      <input
+        type="time"
+        placeholder="Start time"
+        class="border-1 border-red w-2/3 h-10 ml-3 bg-beige"
+      />
+      <label>End time</label>
+      <input
+        type="time"
+        placeholder="End time"
+        class="border-1 border-red w-2/3 h-10 ml-3 bg-beige"
+      />
+      <label>Contactperson</label>
+      <input
+        type="text"
+        placeholder="Contactperson"
+        class="border-1 border-red w-2/3 h-10 ml-3 bg-beige"
+      />
+      <label>Email</label>
+      <input
+        type="email"
+        placeholder="Email"
+        class="border-1 border-red w-2/3 h-10 ml-3 bg-beige"
+      />
+      <label>Phone number</label>
+      <input
+        type="tel"
+        placeholder="Phone number"
+        class="border-1 border-red w-2/3 h-10 ml-3 bg-beige"
+      />
+      <label>Btw number</label>
+      <input
+        type="text"
+        placeholder="Btw number"
+        class="border-1 border-red w-2/3 h-10 ml-3 bg-beige"
+      />
+      <label>Expected visitor / staff count</label>
+      <input
+        type="number"
+        placeholder="Expected visitor / staff count"
+        class="border-1 border-red w-2/3 h-10 ml-3 bg-beige"
+      />
+      <label>Children</label>
+      <input
+        type="radio"
+        placeholder="Children"
+        class="border-1 border-red w-2/3 h-10 ml-3 bg-beige"
+      />Yes
+      <input
+        type="radio"
+        placeholder="Children"
+        class="border-1 border-red w-2/3 h-10 ml-3 bg-beige"
+      />No
+      <label>Maps</label>
+      <input
+        type="file"
+        placeholder="Maps"
+        class="border-1 border-red w-2/3 h-10 ml-3 bg-beige"
+      />
+    </section>
   </div>
 </template>
