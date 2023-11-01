ed6176cc84078dde81687eb0f35f22a4c248d855
DeMeulemeesterMilan <Milan.De.Meulemeester@student.howest.be>
2023-11-01 21:40:59 +0100

    feat: added simple styling

--- packages/pwa/src/views/administration/events/AddEvent.vue
+++ packages/pwa/src/views/administration/events/AddEvent.vue
@@ -1,95 +1,99 @@
 <template>
   <div class="ml-3.5rem md:ml-5rem bg-beige h-100vh">
     <h1 class="title-black">New event</h1>
-    <section class="grid grid-cols-2 mx-auto w-1/2">
-      <label>Event name</label>
+    <section class="grid grid-cols-5 mx-auto w-1/2">
+      <label class="body-black col-span-1">Event name</label>
       <input
         type="text"
         placeholder="Event name"
-        class="border-1 border-red w-2/3 h-10 ml-3 bg-beige"
+        class="border-1 border-black w-2/3 h-10 ml-3 bg-white col-span-4"
       />
-      <label>Address</label>
+      <label class="body-black col-span-1">Address</label>
       <input
         type="text"
         placeholder="Address"
-        class="border-1 border-red w-2/3 h-10 ml-3 bg-beige"
+        class="border-1 border-black w-2/3 h-10 ml-3 bg-white col-span-4"
       />
-      <label>PostalCode</label>
+      <label class="body-black col-span-1">PostalCode</label>
       <input
         type="text"
         placeholder="PostalCode"
-        class="border-1 border-red w-2/3 h-10 ml-3 bg-beige"
+        class="border-1 border-black w-2/3 h-10 ml-3 bg-white col-span-4"
       />
-      <label>City</label>
+      <label class="body-black col-span-1">City</label>
       <input
         type="text"
         placeholder="City"
-        class="border-1 border-red w-2/3 h-10 ml-3 bg-beige"
+        class="border-1 border-black w-2/3 h-10 ml-3 bg-white col-span-4"
       />
-      <label>Dates</label>
+      <label class="body-black col-span-1">Dates</label>
       <input
         type="date"
         placeholder="Dates"
-        class="border-1 border-red w-2/3 h-10 ml-3 bg-beige"
+        class="border-1 border-black w-2/3 h-10 ml-3 bg-white col-span-4"
       />
-      <label>Start time</label>
+      <label class="body-black col-span-1">Start time</label>
       <input
         type="time"
         placeholder="Start time"
-        class="border-1 border-red w-2/3 h-10 ml-3 bg-beige"
+        class="border-1 border-black w-2/3 h-10 ml-3 bg-white col-span-4"
       />
-      <label>End time</label>
+      <label class="body-black col-span-1">End time</label>
       <input
         type="time"
         placeholder="End time"
-        class="border-1 border-red w-2/3 h-10 ml-3 bg-beige"
+        class="border-1 border-black w-2/3 h-10 ml-3 bg-white col-span-4"
       />
-      <label>Contactperson</label>
+      <label class="body-black col-span-1">Contactperson</label>
       <input
         type="text"
         placeholder="Contactperson"
-        class="border-1 border-red w-2/3 h-10 ml-3 bg-beige"
+        class="border-1 border-black w-2/3 h-10 ml-3 bg-white col-span-4"
       />
-      <label>Email</label>
+      <label class="body-black col-span-1">Email</label>
       <input
         type="email"
         placeholder="Email"
-        class="border-1 border-red w-2/3 h-10 ml-3 bg-beige"
+        class="border-1 border-black w-2/3 h-10 ml-3 bg-white col-span-4"
       />
-      <label>Phone number</label>
+      <label class="body-black col-span-1">Phone number</label>
       <input
         type="tel"
         placeholder="Phone number"
-        class="border-1 border-red w-2/3 h-10 ml-3 bg-beige"
+        class="border-1 border-black w-2/3 h-10 ml-3 bg-white col-span-4"
       />
-      <label>Btw number</label>
+      <label class="body-black col-span-1">Btw number</label>
       <input
         type="text"
         placeholder="Btw number"
-        class="border-1 border-red w-2/3 h-10 ml-3 bg-beige"
+        class="border-1 border-black w-2/3 h-10 ml-3 bg-white col-span-4"
       />
-      <label>Expected visitor / staff count</label>
+      <label class="body-black col-span-1"
+        >Expected visitor / staff count</label
+      >
       <input
         type="number"
         placeholder="Expected visitor / staff count"
-        class="border-1 border-red w-2/3 h-10 ml-3 bg-beige"
+        class="border-1 border-black w-2/3 h-10 ml-3 bg-white col-span-4"
       />
-      <label>Children</label>
-      <input
-        type="radio"
-        placeholder="Children"
-        class="border-1 border-red w-2/3 h-10 ml-3 bg-beige"
-      />Yes
-      <input
-        type="radio"
-        placeholder="Children"
-        class="border-1 border-red w-2/3 h-10 ml-3 bg-beige"
-      />No
-      <label>Maps</label>
+      <div class="flex flex-row col-span-5 text-center">
+        <label class="body-black col-span-1">Children</label>
+        <input
+          type="radio"
+          placeholder="Children"
+          class="border-1 border-black bg-white col-span-2"
+        />Yes
+        <input
+          type="radio"
+          placeholder="Children"
+          class="border-1 border-black bg-white col-span-2"
+        />No
+      </div>
+      <label class="body-black col-span-1">Maps</label>
       <input
         type="file"
         placeholder="Maps"
-        class="border-1 border-red w-2/3 h-10 ml-3 bg-beige"
+        class="border-1 border-black w-2/3 h-10 ml-3 bg-white col-span-4"
       />
     </section>
   </div>
