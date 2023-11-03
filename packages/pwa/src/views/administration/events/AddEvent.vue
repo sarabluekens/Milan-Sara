a0a1a96e7124da695c4156860c949a688c606c55
DeMeulemeesterMilan <Milan.De.Meulemeester@student.howest.be>
2023-11-03 17:05:53 +0100

    feat: retrieving the input data

--- packages/pwa/src/views/administration/events/AddEvent.vue
+++ packages/pwa/src/views/administration/events/AddEvent.vue
@@ -1,13 +1,17 @@
 <template>
   <div class="ml-3.5rem md:ml-5rem bg-beige h-100vh">
     <h1 class="title-black">New event</h1>
-    <form class="grid grid-cols-5 mx-auto w-1/2">
+    <form
+      @submit.prevent="handleAddEvent"
+      class="grid grid-cols-5 mx-auto w-1/2"
+    >
       <label class="body-black col-span-1" for="eventname">Event name</label>
       <input
         type="text"
         placeholder="Event name"
         id="eventname"
         class="border-1 border-black w-2/3 h-10 ml-3 bg-white col-span-4 mb-3"
+        v-model="newEvent.name"
       />
       <label class="body-black col-span-1" for="address">Address</label>
       <input
@@ -15,6 +19,7 @@
         placeholder="Address"
         id="address"
         class="border-1 border-black w-2/3 h-10 ml-3 bg-white col-span-4 mb-3"
+        v-model="newEvent.address"
       />
       <label class="body-black col-span-1" for="postalcode">PostalCode</label>
       <input
@@ -22,6 +27,7 @@
         placeholder="PostalCode"
         id="postalcode"
         class="border-1 border-black w-2/3 h-10 ml-3 bg-white col-span-4 mb-3"
+        v-model="newEvent.postalCode"
       />
       <label class="body-black col-span-1" for="city">City</label>
       <input
@@ -29,6 +35,7 @@
         placeholder="City"
         id="city"
         class="border-1 border-black w-2/3 h-10 ml-3 bg-white col-span-4 mb-3"
+        v-model="newEvent.city"
       />
       <label class="body-black col-span-1" for="dates">Dates</label>
       <input
@@ -36,6 +43,7 @@
         placeholder="Dates"
         id="dates"
         class="border-1 border-black w-2/3 h-10 ml-3 subbody-black/80 bg-white col-span-4 mb-3"
+        v-model="newEvent.dates"
       />
       <label class="body-black col-span-1" for="starttime">Start time</label>
       <input
@@ -43,6 +51,7 @@
         placeholder="Start time"
         id="starttime"
         class="border-1 border-black w-2/3 h-10 ml-3 bg-white col-span-4 mb-3 subbody-black/80"
+        v-model="newEvent.startTime"
       />
       <label class="body-black col-span-1" for="endtime">End time</label>
       <input
@@ -50,6 +59,7 @@
         placeholder="End time"
         id="endtime"
         class="border-1 border-black w-2/3 h-10 ml-3 bg-white col-span-4 mb-3 subbody-black/80"
+        v-model="newEvent.endTime"
       />
       <label class="body-black col-span-1" for="contactperson"
         >Contactperson</label
@@ -59,6 +69,7 @@
         placeholder="Contactperson"
         id="contactperson"
         class="border-1 border-black w-2/3 h-10 ml-3 bg-white col-span-4 mb-3"
+        v-model="newEvent.contactPerson"
       />
       <label class="body-black col-span-1" for="email">Email</label>
       <input
@@ -66,6 +77,7 @@
         placeholder="Email"
         id="email"
         class="border-1 border-black w-2/3 h-10 ml-3 bg-white col-span-4 mb-3"
+        v-model="newEvent.email"
       />
       <label class="body-black col-span-1" for="phone-number"
         >Phone number</label
@@ -75,6 +87,7 @@
         placeholder="Phone number"
         id="phone-number"
         class="border-1 border-black w-2/3 h-10 ml-3 bg-white col-span-4 mb-3"
+        v-model="newEvent.phoneNumber"
       />
       <label class="body-black col-span-1" for="btw-number">Btw number</label>
       <input
@@ -82,6 +95,7 @@
         placeholder="Btw number"
         id="btw-number"
         class="border-1 border-black w-2/3 h-10 ml-3 bg-white col-span-4 mb-3"
+        v-model="newEvent.btwNumber"
       />
       <label class="body-black col-span-1" for="expected-visitor-staff-count"
         >Expected visitor / staff count</label
@@ -91,6 +105,7 @@
         placeholder="Expected visitor / staff count"
         id="expected-visitor-staff-count"
         class="border-1 border-black w-2/3 h-10 ml-3 bg-white col-span-4 mb-3"
+        v-model="newEvent.expectedVisitorStaffCount"
       />
       <p class="body-black col-span-1">Children</p>
       <label
@@ -98,7 +113,7 @@
         :key="option.name"
         class="col-span-2 mx-auto mb-3 w-15vw"
       >
-        <input type="radio" :value="option.name" v-model="picked" />
+        <input type="radio" :value="option.name" v-model="newEvent.children" />
         {{ option.name }}
       </label>
       <label class="body-black col-span-1" for="maps">Maps</label>
@@ -107,6 +122,7 @@
         multiple
         id="maps"
         class="border-1 border-black w-2/3 h-10 ml-3 bg-white subbody-black/80 col-span-4 mb-3 file:bg-black file:subbody-white file:h-10"
+        v-on:change="newEvent.maps"
       />
       <button
         class="border-1 border-red bg-red/80 col-span-2 col-start-2 body-black"
@@ -118,11 +134,48 @@
 </template>
 
 <script lang="ts">
+import { ref } from 'vue'
+
 export default {
-  data() {
+  setup() {
+    const newEvent = ref({
+      name: '',
+      address: '',
+      postalCode: '',
+      city: '',
+      dates: '',
+      startTime: '',
+      endTime: '',
+      contactPerson: '',
+      email: '',
+      phoneNumber: '',
+      btwNumber: '',
+      expectedVisitorStaffCount: '',
+      children: false,
+      maps: '',
+    })
+
+    const handleAddEvent = () => {
+      console.log(newEvent.value.name)
+      console.log(newEvent.value.address)
+      console.log(newEvent.value.postalCode)
+      console.log(newEvent.value.city)
+      console.log(newEvent.value.dates)
+      console.log(newEvent.value.startTime)
+      console.log(newEvent.value.endTime)
+      console.log(newEvent.value.contactPerson)
+      console.log(newEvent.value.email)
+      console.log(newEvent.value.phoneNumber)
+      console.log(newEvent.value.btwNumber)
+      console.log(newEvent.value.expectedVisitorStaffCount)
+      console.log(newEvent.value.children)
+      console.log(newEvent.value.maps)
+    }
+
     return {
-      picked: 'no',
       Options: [{ name: 'yes' }, { name: 'no' }],
+      newEvent,
+      handleAddEvent,
     }
   },
 }
