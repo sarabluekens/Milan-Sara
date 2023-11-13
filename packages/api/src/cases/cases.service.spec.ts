4803cc85b568e2610b12edf57965cf52f7bed8d1
sarabluekens <sara.bluekens@skynet.be>
2023-11-13 21:32:37 +0100

    implement case service testing

--- packages/api/src/cases/cases.service.spec.ts
+++ packages/api/src/cases/cases.service.spec.ts
@@ -1,18 +1,29 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { CasesService } from './cases.service';
+import { Test, TestingModule } from '@nestjs/testing'
+import { getRepositoryToken } from '@nestjs/typeorm'
+import { CasesService } from './cases.service'
+import { Repository } from 'typeorm'
+import { Case } from './entities/case.entity'
 
 describe('CasesService', () => {
-  let service: CasesService;
+  let service: CasesService
+  let mockCasesRepository: Repository<Case>
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CasesService],
-    }).compile();
+      providers: [
+        CasesService,
+        {
+          provide: getRepositoryToken(Case),
+          useValue: {},
+        },
+      ],
+    }).compile()
 
-    service = module.get<CasesService>(CasesService);
-  });
+    service = module.get<CasesService>(CasesService)
+    mockCasesRepository = module.get<Repository<Case>>(getRepositoryToken(Case))
+  })
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
-});
+    expect(service).toBeDefined()
+  })
+})
