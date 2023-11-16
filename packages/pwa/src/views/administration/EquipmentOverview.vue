8d9544e9ce1207a1a7a56209f5b34b728e53b112
DeMeulemeesterMilan <Milan.De.Meulemeester@student.howest.be>
2023-11-16 22:16:35 +0100

    feat: getting data out of the database

--- packages/pwa/src/views/administration/EquipmentOverview.vue
+++ packages/pwa/src/views/administration/EquipmentOverview.vue
@@ -2,33 +2,46 @@
   <div class="ml-3.5rem md:ml-5rem bg-beige h-100vh">
     <h1 class="title-black">Equipment overview</h1>
     <section class="flex flex-row mx-auto w-1/2">
-      <div class="border-1 border-red w-1/3 h-10">
-        <p>Dropdown</p>
-      </div>
       <input
         placeholder="Search staff..."
-        class="border-1 border-red w-2/3 h-10 ml-3 bg-beige"
+        class="border-1 border-red w-full mt-3 h-10 ml-3 bg-beige"
       />
     </section>
     <section
       class="grid grid-cols-4 gap-5 mx-auto my-10 w-1/2 text-center justify-between"
     >
-      <div class="text-black w-56 h-18 bg-white">
-        <p class="body-black">Pieter Post</p>
-        <p class="subbody-black">Paramedic</p>
-      </div>
-      <div class="text-black w-56 h-18 bg-white">
-        <p class="body-black">Pieter Post</p>
-        <p class="subbody-black">Paramedic</p>
-      </div>
-      <div class="text-black w-56 h-18 bg-white">
-        <p class="body-black">Pieter Post</p>
-        <p class="subbody-black">Paramedic</p>
-      </div>
-      <div class="text-black w-56 h-18 bg-white">
-        <p class="body-black">Pieter Post</p>
-        <p class="subbody-black">Paramedic</p>
+      <div v-if="equipmentLoading">Loading</div>
+      <div v-if="equipmentError">{{ equipmentError }}</div>
+      <div
+        v-if="equipments"
+        v-for="equipment in equipments.equipments"
+        class="text-black w-56 h-18 bg-white"
+      >
+        <p class="body-black">{{ equipment.name }}</p>
+        <p class="subbody-black">{{ equipment.category }}</p>
+        <p class="subbody-black">Amount: {{ equipment.totalStock }}</p>
       </div>
     </section>
   </div>
 </template>
+
+<script lang="ts">
+import { useQuery } from '@vue/apollo-composable'
+import { ALL_EQUIPMENT } from '@/graphql/equipment.query'
+
+export default {
+  setup() {
+    const {
+      result: equipments,
+      loading: equipmentLoading,
+      error: equipmentError,
+    } = useQuery(ALL_EQUIPMENT)
+    console.log(equipments)
+    return {
+      equipments: equipments,
+      equipmentLoading,
+      equipmentError,
+    }
+  },
+}
+</script>
