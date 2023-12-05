2c9d9dd38fc4782e5f8a839e0185367558a35c4f
DeMeulemeesterMilan <Milan.De.Meulemeester@student.howest.be>
2023-12-05 17:03:57 +0100

    feat: updated the EquipmentCard

--- packages/pwa/src/components/EquipmentCard.vue
+++ packages/pwa/src/components/EquipmentCard.vue
@@ -1,8 +1,29 @@
 <template>
-  <div class="text-black w-56 h-18 bg-white">
+  <div
+    v-if="equipment.totalStock > 0"
+    class="w-56 h-18 border-2 border-red rounded-md h-64"
+  >
     <p class="body-black">{{ equipment.name }}</p>
     <p class="subbody-black">{{ equipment.category }}</p>
-    <p class="subbody-black">Amount: {{ equipment.totalStock }}</p>
+    <div class="w-3 h-40"></div>
+    <div class="flex justify-end mr-1">
+      <div class="flex bg-red w-28 h-9 rounded-lg justify-center items-center">
+        <p class="subbody-white">{{ equipment.totalStock }} left</p>
+      </div>
+    </div>
+  </div>
+  <div
+    v-if="equipment.totalStock === 0"
+    class="w-56 h-18 border-2 border-red rounded-md h-64 opacity-35"
+  >
+    <p class="body-black">{{ equipment.name }}</p>
+    <p class="subbody-black">{{ equipment.category }}</p>
+    <div class="w-3 h-40"></div>
+    <div class="flex justify-end mr-1">
+      <div class="flex bg-red w-28 h-9 rounded-lg justify-center items-center">
+        <p class="subbody-white">{{ equipment.totalStock }} left</p>
+      </div>
+    </div>
   </div>
 </template>
 
