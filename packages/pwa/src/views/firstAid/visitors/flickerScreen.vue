9bc9f607c908cc9699a8c9ee4e23d5b7b2a2caac
sarabluekens <sara.bluekens@skynet.be>
2023-12-18 00:55:51 +0100

    feature: flicker screen responsive

--- packages/pwa/src/views/firstAid/visitors/flickerScreen.vue
+++ packages/pwa/src/views/firstAid/visitors/flickerScreen.vue
@@ -1,35 +1,42 @@
 <template>
-  <section class=" bg-beige h-100vh">
-    <section class="ml-5rem">
-    <div class="w-80vw flex items-center justify-center flex-col">
-      <div v-if="!flickering" class="w-70% py-3rem">
-        <p class="body-red">
-            <h1 class="title-red">Help is almost with you</h1>
+  <section class="bg-beige h-100vh">
+    <section class="sm:ml-5rem">
+      <div class="w-95vw flex items-center justify-center flex-col">
+        <div v-if="!flickering">
+          <h1 class="title-red">Help is almost with you</h1>
 
-          Press this button to make your screen flash bright red. This will help
-          you stand out for the caregiver. Careful, epilepsy warning
-        </p>
-        <button
-          class="bg-red rounded rounded-md px-10 py-3 body-white self-end mt-3vh"
-          @click="handleClick"
-        >
-          Flicker screen
-        </button>
+          <p class="body-black w-2/3 text-center m-auto py-3rem">
+            Press this button to make your screen flash bright red. This will
+            help you stand out for the caregiver. Careful, epilepsy warning
+          </p>
+          <div class="w-1/4 m-auto">
+            <button
+              class="bg-red rounded rounded-md px-10 py-3 body-white"
+              @click="handleClick"
+            >
+              Flicker screen
+            </button>
+          </div>
+        </div>
       </div>
-    </div>
-    <div v-if="flickering">
+      <div
+        v-if="flickering"
+        class="w-95vw flex items-center justify-center flex-col"
+      >
         <button
-          class="bg-white rounded rounded-md px-10 py-3 body-red self-end mt-3vh absolute top-2/5 left-3/7 z-1"
+          class="bg-white rounded rounded-md px-10 py-3 body-red absolute top-2/5 z-1"
           @click="handleClick"
         >
           Stop flickering screen
         </button>
-        <div class="animate-[flash_0.7s_ease-in-out_infinite] w-95vw h-100vh bg-red m-auto z-0">
-            <img src="/whiteCross.svg" alt="" />
+        <div
+          class="animate-[flash_0.7s_ease-in-out_infinite] w-95vw h-100vh bg-red m-auto z-0"
+        >
+          <img src="/whiteCross.svg" alt="" />
         </div>
-    </div>
+      </div>
+    </section>
   </section>
-</section>
 </template>
 <script setup lang="ts">
 import { ref } from 'vue'
