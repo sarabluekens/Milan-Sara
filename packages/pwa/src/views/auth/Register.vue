5a5e9ab7a014254d8c9c483c7dd06b82940bcfeb
DeMeulemeesterMilan <Milan.De.Meulemeester@student.howest.be>
2023-12-20 21:59:39 +0100

    feat: register page responsive

--- packages/pwa/src/views/auth/Register.vue
+++ packages/pwa/src/views/auth/Register.vue
@@ -1,78 +1,83 @@
 <template>
-  <form @submit.prevent="handleRegister" class="w-full">
-    <h1 class="text-4xl font-bold tracking-wider">Register</h1>
-    <p class="text-neutral-500 mb-4">
-      Create an account and keep track of your events.
-    </p>
-
-    <div v-if="error">
-      <p class="text-red-600">{{ error.message }}</p>
-    </div>
-
-    <div class="mt-6">
-      <label
-        for="nickname"
-        class="text-md block font-semibold tracking-wider text-gray-700 dark:text-gray-200"
-      >
-        Name
-      </label>
-      <input
-        type="text"
-        name="nickname"
-        id="nickname"
-        class="mt-1 block w-full rounded-md border-2 border-gray-300 p-2 focus:outline-none focus-visible:ring-2 dark:border-neutral-600 dark:bg-neutral-700 dark:text-neutral-50 focus-visible:border-blue-500 focus-visible:ring-blue-400"
-        v-model="newUser.name"
-      />
-    </div>
-
-    <div class="mt-6">
-      <label
-        for="email"
-        class="text-md block font-semibold tracking-wider text-gray-700 dark:text-gray-200"
-      >
-        Email address
-      </label>
-      <input
-        type="email"
-        name="email"
-        id="email"
-        class="mt-1 block w-full rounded-md border-2 border-gray-300 p-2 focus:outline-none focus-visible:border-blue-500 focus-visible:ring-2 focus-visible:ring-blue-400 dark:border-neutral-600 dark:bg-neutral-700 dark:text-neutral-50"
-        v-model="newUser.email"
-      />
-    </div>
-
-    <div class="mt-6">
-      <label
-        for="password"
-        class="text-md block font-semibold tracking-wider text-gray-700 dark:text-gray-200"
-      >
-        Password
-      </label>
-      <input
-        type="password"
-        name="password"
-        id="password"
-        class="mt-1 block w-full rounded-md border-2 border-gray-300 p-2 focus:outline-none focus-visible:border-blue-500 focus-visible:ring-2 focus-visible:ring-blue-400 dark:border-neutral-600 dark:bg-neutral-700 dark:text-neutral-50"
-        v-model="newUser.password"
-      />
-    </div>
-
-    <button
-      :disabled="addUserLoading"
-      class="mt-6 w-full rounded-md border-2 border-blue-500 bg-blue-500 py-2 px-4 font-semibold text-white hover:bg-blue-600 focus:outline-none focus-visible:border-blue-300 focus-visible:bg-blue-600 focus-visible:ring-2 focus-visible:ring-blue-300 flex items-center justify-center disabled:bg-blue-200"
+  <div
+    class="flex flex-col items-center sm:h-100vh h-90vh md:ml-5rem lg:ml-0rem"
+  >
+    <h1 class="title-red my-3 md:my-0 lg:my-5">Register</h1>
+    <form
+      @submit.prevent="handleRegister"
+      class="flex mx-auto justify-center flex-col"
     >
-      <template v-if="!addUserLoading">Register</template>
-      <Loader2 v-else class="animate-spin" />
-    </button>
-    <div class="flex justify-center">
-      <RouterLink
-        class="mt-3 inline-block rounded text-center text-sm text-blue-500 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-200"
-        to="/auth/login"
+      <div v-if="error">
+        <p class="text-red-600">{{ error.message }}</p>
+      </div>
+
+      <div
+        class="border-7 border-red p-2 rounded-xl w-17rem md:w-30rem lg:w-40rem lg:h-32rem"
       >
-        Already have an account?
-      </RouterLink>
-    </div>
-  </form>
+        <div
+          class="border-2 border-red p-4 md:p-9 rounded-lg lg:w-38rem lg:h-30rem"
+        >
+          <label
+            for="nickname"
+            class="subbody-red block tracking-wider text-red"
+          >
+            Name
+          </label>
+          <input
+            type="text"
+            name="nickname"
+            id="nickname"
+            placeholder="Nickname"
+            class="mt-1 block w-52 md:w-full h-12 placeholder:subbody-pink subbody-black rounded-md bg-beige p-2 focus:outline-none focus-visible:border-blue-500 focus-visible:ring-2 focus-visible:ring-blue-400"
+            v-model="newUser.name"
+          />
+          <label
+            for="email"
+            class="subbody-red block tracking-wider text-red mt-6 md:mt-8"
+          >
+            Email address
+          </label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="Email address"
+            class="mt-1 block w-52 md:w-full h-12 placeholder:subbody-pink subbody-black rounded-md bg-beige p-2 focus:outline-none focus-visible:border-blue-500 focus-visible:ring-2 focus-visible:ring-blue-400"
+            v-model="newUser.email"
+          />
+          <label
+            for="password"
+            class="subbody-red block tracking-wider text-red mt-6 md:mt-8"
+          >
+            Password
+          </label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="Password"
+            class="mt-1 block w-52 md:w-full h-12 placeholder:subbody-pink subbody-black rounded-md bg-beige p-2 focus:outline-none focus-visible:border-blue-500 focus-visible:ring-2 focus-visible:ring-blue-400"
+            v-model="newUser.password"
+          />
+          <div class="flex justify-center items-center flex-col">
+            <button
+              :disabled="addUserLoading"
+              class="mt-7 md:mt-11 w-52 lg:w-72 rounded-md body-white border-2 border-red bg-red py-2 px-4 font-semibold hover:bg-blue-600 focus:outline-none focus-visible:border-blue-300 focus-visible:bg-blue-600 focus-visible:ring-2 focus-visible:ring-blue-300"
+            >
+              <template v-if="!addUserLoading">Register</template>
+              <Loader2 v-else class="animate-spin" />
+            </button>
+            <RouterLink
+              class="mt-3 underline inline-block rounded subbody-black hover:underline focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-200"
+              to="/auth/login"
+            >
+              Already have an account?
+            </RouterLink>
+          </div>
+        </div>
+      </div>
+    </form>
+  </div>
 </template>
 
 <script lang="ts">
