b0f12f061cb4224027256d03fee1862afe25d615
sarabluekens <sara.bluekens@skynet.be>
2023-12-21 22:28:26 +0100

    feature: seeding finishing touches

--- packages/api/src/seed/seed.service.ts
+++ packages/api/src/seed/seed.service.ts
@@ -140,7 +140,7 @@ export class SeedService {
   }
 
   async deleteAllVictims(): Promise<void> {
-    return this.eventsService.truncate()
+    return this.victimsService.truncate()
   }
 
   async addCasesFromJson(): Promise<Case[]> {
